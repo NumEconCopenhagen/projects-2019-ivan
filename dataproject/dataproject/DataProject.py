0d9deabe90a264b5751b578650fed6973a664a1a
jonaswh <pjr395@alumni.ku.dk>
2019-04-05 00:00:17 +0200

    Done

--- dataproject/dataproject/DataProject.py
+++ dataproject/dataproject/DataProject.py
@@ -1,11 +1,3 @@
-#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
-import os
-try:
-	os.chdir(os.path.join(os.getcwd(), 'dataproject\dataproject'))
-	print(os.getcwd())
-except:
-	pass
-
 #%%
 # Importing packages
 
